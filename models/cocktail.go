1e50f76d20cbf2c4b9cfbdefaf570eed4960133d
Facundo Caricato <caricatitof@gmail.com>
2025-08-08 15:36:33 -0300

    Get Cocktails

--- models/cocktail.go
+++ models/cocktail.go
@@ -1,13 +1,14 @@
 package models
 
 type Cocktail struct {
-	ID          int      `json:"id"`
-	Nombre      string   `json:"nombrecoctel"`
-	Description string   `json:"descripcoctel"`
-	Vaso        string   `json:"idtipoVaso"`
-	Hielo       string   `json:"idtipoHielo"`
-	Ingredients []string `json:"ingredients"`
-	Metodo      string   `json:"idmetodo"`
-	Garnish     []string `json:"garnish"`
-	Activo      bool     `json:"isactivecocktail"`
+	ID          int           `json:"id"`
+	Nombre      string        `json:"nombrecoctel"`
+	Description string        `json:"descripcoctel"`
+	Vaso        string        `json:"idtipoVaso"`
+	Hielo       string        `json:"idtipoHielo"`
+	Canthielo   int           `json:"canthielo"`
+	Ingredients []Ingredients `json:"ingredients"`
+	Metodo      string        `json:"idmetodo"`
+	Garnish     []Garnish     `json:"garnish"`
+	Activo      bool          `json:"isactivecocktail"`
 }
